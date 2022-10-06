66a9425bb2c7db91cf3b542e425f8ad09ebe1981
Alexa Luborsky <alexaluborsky@gmail.com>
2022-10-06 13:37:41 -0400

    Add red on

--- arduino_receiver/arduino_receiver.ino
+++ arduino_receiver/arduino_receiver.ino
@@ -114,6 +114,7 @@ void loop()
       digitalWrite(BLUE2, HIGH);
       delay(1000);
       turnOffAll(); 
+      digitalWrite(RED1, HIGH);
     }
     else if (letter.equals("h")) {
       //Turn yellows on first, then short blue LED. 
