3c8cc95a0b25be0f91c7f25992ab9ab99563329c
Alexa Luborsky <alexaluborsky@gmail.com>
2022-10-06 13:35:26 -0400

    Delete delay

--- arduino_receiver/arduino_receiver.ino
+++ arduino_receiver/arduino_receiver.ino
@@ -114,7 +114,6 @@ void loop()
       digitalWrite(BLUE2, HIGH);
       delay(1000);
       turnOffAll(); 
-      delay(1000);
     }
     else if (letter.equals("h")) {
       //Turn yellows on first, then short blue LED. 
